9078bf28fce41ed8183683a1312e6198a4fa1e24
nagusingh <vnagusingh@gmail.com>
2025-03-13 21:15:18 +0530

    initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    public static void printSomething() {
+        System.out.println("Print some thing");
+    }
+}
